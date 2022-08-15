1709409237542f0608633be400cff552b29eccc4
masce <mascena19@gmail.com>
2022-08-15 01:26:27 -0300

    Adicionando Swagger

--- src/main/java/br/com/rodrigoMascena/parking/controller/mapper/ParkingMapper.java
+++ src/main/java/br/com/rodrigoMascena/parking/controller/mapper/ParkingMapper.java
@@ -1,11 +1,11 @@
 package br.com.rodrigoMascena.parking.controller.mapper;
 
+import br.com.rodrigoMascena.parking.controller.dto.ParkingCreateDTO;
 import br.com.rodrigoMascena.parking.controller.dto.ParkingDTO;
 import br.com.rodrigoMascena.parking.model.Parking;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
-import javax.management.modelmbean.ModelMBean;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,12 +14,19 @@ public class ParkingMapper {
 
     private static final ModelMapper MODEL_MAPPER = new ModelMapper();
 
-    public ParkingDTO parkingDTO(Parking parking){
+    public ParkingDTO toParkingDTO(Parking parking){
         return MODEL_MAPPER.map(parking, ParkingDTO.class);
     }
 
     public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
-        return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+        return parkingList.stream().map(this::toParkingDTO).collect(Collectors.toList());
     }
 
+    public Parking toParking(ParkingCreateDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+    }
+
+    public Parking toParkingCreate(ParkingCreateDTO dto) {
+        return MODEL_MAPPER.map(dto, Parking.class);
+    }
 }
--- src/main/java/br/com/rodrigoMascena/parking/service/ParkingService.java
+++ src/main/java/br/com/rodrigoMascena/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package br.com.rodrigoMascena.parking.service;
 import br.com.rodrigoMascena.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,11 @@ public class ParkingService {
     
     static {
         String id = getUUID();
+        String id1 = getUUID();
         Parking parking = new Parking(id, "DDD-1111", "SC", "CELTA", "PRETO");
+        Parking parking1 = new Parking(id1, "DDE-2222", "GO", "HB20", "BRANCO");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll(){
@@ -27,4 +31,15 @@ public class ParkingService {
     }
 
 
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
+    }
 }
